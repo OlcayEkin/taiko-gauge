cbac64ed98a0109236821114276e7b8ea62c70eb
OlcayEkin <olcayekinn@gmail.com>
2019-11-12 11:58:16 +0300

    Fixing the running headless test problem

--- tests/step_implementation.js
+++ tests/step_implementation.js
@@ -1,14 +1,7 @@
 /* globals gauge*/
 const { openBrowser, goto, click, $, write, into, press  } = require('taiko');
 
-beforeScenario(async() => await openBrowser({args: [ 
-    '--disable-gpu',
-    '--disable-dev-shm-usage',
-    '--disable-setuid-sandbox',
-    '--no-first-run',
-    '--no-sandbox',
-    '--no-zygote']}
-));
+beforeScenario(async() => await openBrowser({headless: false}));
 
 afterSuite(async () => {
     await closeBrowser();
@@ -24,7 +17,7 @@ step("Choose <cityCode>", async (cityCodeGiven) => {
 
 step("Search some <food> for the <city>", async (food, city) => {
         await write(food,$(".search-box"));
-        await click($(".select2-search__field"));
+        await click($("#select2-ys-areaSelector-container"));
         await write(city, $(".select2-search__field"));
         await press("Enter");
 });
